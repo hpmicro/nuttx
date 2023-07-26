e89ad1bb57a632b4751df221ddf5611de9e79ff9
Runcheng Lu <runcheng.lu@hpmicro.com>
2023-07-26 14:20:54 +0800

    nuttx: drivers: can: fix loop on can_write api when enable canfd
    -  fix loop on can_write api when enable canfd
    
    Signed-off-by: Runcheng Lu <runcheng.lu@hpmicro.com>

--- drivers/can/can.c
+++ drivers/can/can.c
@@ -761,7 +761,7 @@ static ssize_t can_write(FAR struct file *filep, FAR const char *buffer,
    * shorter than the minimum.
    */
 
-  while (((ssize_t)buflen - nsent) >= CAN_MSGLEN(0))
+  while (((ssize_t)buflen - nsent) >= (ssize_t)CAN_MSGLEN(0))
     {
       /* Check if adding this new message would over-run the drivers ability
        * to enqueue xmit data.
