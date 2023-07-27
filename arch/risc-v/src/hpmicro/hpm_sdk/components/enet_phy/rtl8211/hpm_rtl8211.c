ee665ff926259b889e3e588fba9ece8aa4b6dceb
Zhihong Chen <zhihong.chen@hpmicro.com>
2023-07-27 12:33:41 +0800

    hpmicro: enet_phy: fix build warning
    - fix build warning
    
    Signed-off-by: Zhihong Chen <zhihong.chen@hpmicro.com>

--- arch/risc-v/src/hpmicro/hpm_sdk/components/enet_phy/rtl8211/hpm_rtl8211.c
+++ arch/risc-v/src/hpmicro/hpm_sdk/components/enet_phy/rtl8211/hpm_rtl8211.c
@@ -52,7 +52,7 @@ void rtl8211_reset(ENET_Type *ptr)
 void rtl8211_basic_mode_default_config(ENET_Type *ptr, rtl8211_config_t *config)
 {
     config->loopback         = 0;                            /* Disable PCS loopback mode */
-    #if __DISABLE_AUTO_NEGO
+    #if defined(__DISABLE_AUTO_NEGO) && __DISABLE_AUTO_NEGO
     config->auto_negotiation = false;                        /* Disable Auto-Negotiation */
     config->speed            = enet_phy_port_speed_100mbps;
     config->duplex           = enet_phy_duplex_full;
