36b7ce9b3a8db426aa98aad4a12666d2fbd4457c
Zhihong Chen <zhihong.chen@hpmicro.com>
2025-07-02 11:54:40 +0800

    hpmicro: update hpm_sdk to v1.10.0
    Signed-off-by: Zhihong Chen <zhihong.chen@hpmicro.com>

--- arch/risc-v/src/hpmicro/hpm_sdk/drivers/inc/hpm_sdk_version.h
+++ arch/risc-v/src/hpmicro/hpm_sdk/drivers/inc/hpm_sdk_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 HPMicro
+ * Copyright (c) 2025 HPMicro
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -11,14 +11,14 @@
 /* #undef SDK_VERSION_CODE */
 #define SDK_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
-#define SDKVERSION          0x1040000
-#define SDK_VERSION_NUMBER  0x10400
+#define SDKVERSION          0x1100000
+#define SDK_VERSION_NUMBER  0x11000
 #define SDK_VERSION_MAJOR   1
-#define SDK_VERSION_MINOR   4
+#define SDK_VERSION_MINOR   10
 #define SDK_PATCHLEVEL      0
-#define SDK_VERSION_STRING  "1.4.0"
+#define SDK_VERSION_STRING  "1.10.0"
 
-#define BUILD_VERSION          v1.4.0
+#define BUILD_VERSION          v1.10.0
 
 
 #endif /* HPM_SDK_VERSION_H */
