9793b189997dc1f0ce3a572982147a5e6019d7f2
Zhihong Chen <zhihong.chen@hpmicro.com>
2025-08-12 19:04:46 +0800

    hpmicro: hpm_i2c_master: delelte I2C_STATUS_BUSBUSY_MASK polling chek
    Signed-off-by: Zhihong Chen <zhihong.chen@hpmicro.com>

--- arch/risc-v/src/hpmicro/hpm_i2c_master.c
+++ arch/risc-v/src/hpmicro/hpm_i2c_master.c
@@ -530,8 +530,6 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
           if(sta == status_success)
           {
             nxsem_wait_uninterruptible(&priv->txrxsem);
-            while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-            };
             memcpy(msgs[0].buffer, priv->txrxbuf, msgs[0].length);
           }
         }
@@ -542,8 +540,6 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
           if(sta == status_success)
           {
             nxsem_wait_uninterruptible(&priv->txrxsem);
-            while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-            };
           }
         }
     }
@@ -569,8 +565,6 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
               if(sta == status_success)
               {
                 nxsem_wait_uninterruptible(&priv->txrxsem);
-                while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                };
                 memcpy(msgs[1].buffer, priv->txrxbuf, msgs[1].length);
               }
             }
@@ -584,15 +578,11 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
                 while (!(i2c_get_status(priv->base) & I2C_STATUS_CMPL_MASK)) {
                 };
                 i2c_clear_status(priv->base, I2C_STATUS_CMPL_MASK);
-                while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                };
 
                 sta = hpm_i2c_master_read_nonblocking(priv->i2c_context, msgs[1].addr, priv->txrxbuf, msgs[1].length);
                 if(sta == status_success)
                 {
                   nxsem_wait_uninterruptible(&priv->txrxsem);
-                  while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                  };
                   memcpy(msgs[1].buffer, priv->txrxbuf, msgs[1].length);
                 }
               }
@@ -617,8 +607,6 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
               if(sta == status_success)
               {
                 nxsem_wait_uninterruptible(&priv->txrxsem);
-                while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                };
               }
             }
           else
@@ -628,16 +616,12 @@ static int hpm_i2c_transfer_dma(struct i2c_master_s *dev,
               if(sta == status_success)
               {
                 nxsem_wait_uninterruptible(&priv->txrxsem);
-                while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                };
 
                 memcpy(priv->txrxbuf, msgs[1].buffer, msgs[1].length);
                 sta = hpm_i2c_master_write_nonblocking(priv->i2c_context, msgs[1].addr, priv->txrxbuf, msgs[1].length);
                 if(sta == status_success)
                 {
                   nxsem_wait_uninterruptible(&priv->txrxsem);
-                  while ((i2c_get_status(priv->base) & I2C_STATUS_BUSBUSY_MASK)) {
-                  };
                 }
               }
             }
